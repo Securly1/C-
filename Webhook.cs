81503ca43d98887b85cdb4b5cc9136e28ffcffab
Securly <91905863+Securly1@users.noreply.github.com>
2021-10-19 13:13:02 -0400

    Update Webhook.cs

--- Webhook.cs
+++ Webhook.cs
@@ -1,1 +1,22 @@
 using System;
+using System.IO;
+using System.Requests;
+
+
+public static void main(string[] args)
+{
+  string Hook = "";
+  string TEXT_VAR = "";
+  
+  public void SendMess(string Token, string TEXT, string USER)
+  {
+    request.AddItem()
+    {
+      Constant = "Constant";
+    }
+    request.AddItem()
+    {
+      Username = "Username";
+    }
+  }
+}
